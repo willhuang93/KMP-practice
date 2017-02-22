8aba5180dc9b1ae521c1de8a089f91cba851d395
willhuang93 <negative134@gmail.com>
2017-02-21 18:33:33 -0800

    finished KMP implementation

--- KMP.java
+++ KMP.java
@@ -1,12 +1,17 @@
 public class KMP {
 
 	public static void main (String[] args) {
-		char[] pattern = {'a', 'b', 'c', 'd', 'a', 'b', 'c', 'a'};
+		String textString = "abxabcabcaby";
+		String patternString = "abcaby";
+		char[] text = textString.toCharArray();
+		char[] pattern = patternString.toCharArray();
 		int[] precomputedTable = makeTable(pattern);
 		for(int i : precomputedTable) {
 			System.out.printf("%d ", i);
 		}
 		System.out.println();
+
+		System.out.println(findString(text, pattern));
 	}
 
 	public static int[] makeTable(char[] pattern) {
@@ -29,7 +34,23 @@ public class KMP {
 		return table;
 	}
 
-	
+	public static int findString(char[] text, char[] pattern) {
+		int[] table = makeTable(pattern);
+		int textIndex = 0;
+		int patternIndex = 0;
+
+		while (textIndex < text.length && patternIndex < pattern.length) {
+			if (text[textIndex] == pattern[patternIndex]) {
+				textIndex++;
+				patternIndex++;
+			} else if (patternIndex != 0) 
+				patternIndex = table[patternIndex - 1];
+			else
+				textIndex++;
+		}
+
+		return patternIndex == pattern.length ? textIndex - patternIndex : -1;
+	}
 
 
 }
